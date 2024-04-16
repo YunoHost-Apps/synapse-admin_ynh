754693b325f6416fa28434489a674a074d7a09d7
Alexandre Aubin <alex.aubin@mailoo.org>
2024-04-16 18:19:27 +0200

    Zgrmlbl

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-synapse_domain=$(ynh_app_setting_get --app="$synapse_app" --key=domain)
-synapse_port=$(ynh_app_setting_get --app="$synapse_app" --key=port_synapse)
-if [[ -z "$synapse_port" ]]
-then
-    # The setting was synapse_port in older versions of the app
-    synapse_port=$(ynh_app_setting_get --app="$synapse_app" --key=synapse_port)
-    if [[ -z "$synapse_port" ]]
-        ynh_die --message="Unable to find synapse port ?!"
-    fi
-fi
-
 _add_synapse_endpoint_nginx_config() {
+
+    synapse_domain=$(ynh_app_setting_get --app="$synapse_app" --key=domain)
+    synapse_port=$(ynh_app_setting_get --app="$synapse_app" --key=port_synapse)
+    if [[ -z "$synapse_port" ]]
+    then
+        # The setting was synapse_port in older versions of the app
+        synapse_port=$(ynh_app_setting_get --app="$synapse_app" --key=synapse_port)
+        if [[ -z "$synapse_port" ]]
+        then
+            ynh_die --message="Unable to find synapse port ?!"
+        fi
+    fi
+
     # In case the domain changed for some reason
-    old_endpoint_confs_for_this_app="$(ls /etc/nginx/conf.d/!($synapse_domain).d/$app.endpoint.conf)"
+    old_endpoint_confs_for_this_app="$(ls /etc/nginx/conf.d/!($synapse_domain).d/$app.endpoint.conf 2>/dev/null)"
     for FILE in $old_endpoint_confs_for_this_app
     do
         ynh_delete_file_checksum --file="$FILE"
